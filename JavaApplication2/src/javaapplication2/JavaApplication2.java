fd6a20f5936d6038791f7f19a6fbc5ef145699bc
hani momeni <hani.momniniya@gmail.com>
2014-01-20 04:03:49 +0330

    C ezaf shod

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -17,12 +17,12 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int x=calc(2, 3);
+        int x=calc(2, 3, 4);
         System.out.println(x);
     }
-    public static int calc(int a,int b)
+    public static int calc(int a,int b,int c)
     {
-        int sum= a+b;
+        int sum= a+b+c;
         return sum;
         
     }
